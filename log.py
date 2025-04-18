c83f5e830bf57ce0da6cc9523785df7265231a61
Grzegorz Ożański <greggor@gmail.com>
2025-04-18 12:04:36 +0200

    Documentation added

--- log.py
+++ log.py
@@ -1,7 +1,20 @@
+"""
+    Browser logging setup
+"""
 import logging
+from logging import Logger
 
 
-def setup_logging(name, level, formatting="%(levelname)s:%(name)s %(asctime)s %(message)s"):
+def setup_logging(name: str, level: str, formatting: str = "%(levelname)s:%(name)s %(asctime)s %(message)s") -> Logger:
+    """
+    Setup browser logging
+
+    :param name: log name
+    :param level: log level name
+    :param formatting: log format
+
+    :return: logger object
+    """
     log = logging.getLogger(name)
     log.setLevel(level)
     ch = logging.StreamHandler()
