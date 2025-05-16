12988c30d7f5d75a4a0a45eff212df4c0d3ecb82
Grzegorz Ożański <greggor@gmail.com>
2025-05-16 16:18:13 +0200

    Remove unnecessary filtering

--- chromedownloader.py
+++ chromedownloader.py
@@ -15,10 +15,31 @@ CHROME_API_ENDPOINT_URL = \
     'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json'
 
 
+def unpack(content: bytes, archive_dir: str, output_dir: str | Path) -> None:
+    """
+    Unpack downloaded zip archive replacing internal root dir with provided one
+
+    :param content: compressed ZIP content
+    :param archive_dir: archive directory to be replaced with :param output_dir
+    :param output_dir: value to replace the :param archive_dir with when unpacking the archive
+    """
+    with zipfile.ZipFile(io.BytesIO(content)) as zip_file:
+        for member in zip_file.namelist():
+            if member.startswith(archive_dir + "/") and not member.endswith("/"):
+                # Replace archive_dir prefix with target_dir one
+                relative_path = member[len(archive_dir) + 1:]
+                target_path = Path(output_dir, relative_path)
+                os.makedirs(target_path.parent, exist_ok=True)
+                with zip_file.open(member) as src, open(target_path, "wb") as dst:
+                    # supress the warning as the result of open() actually is a BufferedWriter
+                    shutil.copyfileobj(src, dst)  # type: ignore[arg-type]
+
+
 class ChromeDownloader:
     """
         Download and unpack latest stable Chrome driver and Chrome
     """
+
     class Component(StrEnum):
         """
         Component name
@@ -29,12 +50,6 @@ class ChromeDownloader:
     def __init__(self, platform_name: str) -> None:
         self.platform_name = platform_name
         self._downloads = None
-        self.unpack_filters = {
-            # unpack all
-            ChromeDownloader.Component.CHROME: '',
-            # umpack only chromedriver executable
-            ChromeDownloader.Component.CHROMEDRIVER: f'chromedriver-{self.platform_name}/chromedriver'
-        }
 
     @property
     def downloads(self) -> dict[str, Any]:
@@ -77,28 +92,4 @@ class ChromeDownloader:
         response.raise_for_status()
         archive_dir = f'{what}-{self.platform_name}'
 
-        self._unpack(response.content, archive_dir, where, self.unpack_filters[what])
-
-    def _unpack(self, content: bytes, archive_dir: str, output_dir: str | Path, name_filter: str) -> None:
-        """
-        Unpack downloaded Chrome driver/Chrome zip
-
-        :param content: compressed ZIP content
-        :param archive_dir: archive directory to be replaced with :param output_dir
-        :param output_dir: value to replace the :param archive_dir with when unpacking the archive
-        :param name_filter: if provided, only items starting with its value will be unpacked
-        """
-        with zipfile.ZipFile(io.BytesIO(content)) as zip_file:
-            members = (
-                (name for name in zip_file.namelist() if name.startswith(name_filter))
-                if name_filter else zip_file.namelist()
-            )
-            for member in members:
-                if member.startswith(archive_dir + "/") and not member.endswith("/"):
-                    # Replace archive_dir prefix with target_dir one
-                    relative_path = member[len(archive_dir) + 1:]
-                    target_path = Path(output_dir, relative_path)
-                    os.makedirs(target_path.parent, exist_ok=True)
-                    with zip_file.open(member) as src, open(target_path, "wb") as dst:
-                        # supress the warning as the result of open() actually is a BufferedWriter
-                        shutil.copyfileobj(src, dst)  # type: ignore[arg-type]
+        unpack(response.content, archive_dir, where)
