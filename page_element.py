b93aedc8f3035c75840716c2fb56eeee94fe6aab
Grzegorz Ożański <greggor@gmail.com>
2026-01-01 17:17:52 +0100

    String representation

--- page_element.py
+++ page_element.py
@@ -5,3 +5,6 @@ class PageElement:
     """Element locator used for finding inputs and buttons in the page."""
     by: str
     selector: str
+
+    def __repr__(self):
+        return f'({self.by}) {self.selector}'
