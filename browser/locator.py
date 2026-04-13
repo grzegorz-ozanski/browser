9a2a880b55339a3b480dea7089a418ab52e25548
Grzegorz Ożański <greggor@gmail.com>
2026-04-13 07:37:48 +0200

    Fix type annotation in Locator

--- browser/locator.py
+++ browser/locator.py
@@ -2,6 +2,7 @@
     Web page element locator module
 """
 from dataclasses import dataclass
+from typing import Self
 
 from selenium.webdriver.common.by import By
 
@@ -12,14 +13,14 @@ class Locator:
     type: str
     value: str
 
-    def parent(self) -> Locator:
+    def parent(self) -> Self:
         """
         Returns the parent element of the locator for XPATH locators
         :return: element's parent
         """
         if self.type != By.XPATH:
             raise RuntimeError(f'Cannot find parent for non-XPATH locator {self}')
-        return Locator(self.type, f'{self.value}/..')
+        return self.__class__(self.type, f'{self.value}/..')
 
     def __repr__(self) -> str:
         """String representation of the locator."""
